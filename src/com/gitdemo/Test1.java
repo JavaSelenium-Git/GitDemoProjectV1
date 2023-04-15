304fbf3c2223cd98a895098cc159f8031201434e
1309 <1309@MBTLT34.smartcliff.loc>
2023-04-15 22:40:18 +0530

    First Commit...

---
+++ src/com/gitdemo/Test1.java
@@ -0,0 +1,15 @@
+package com.gitdemo;
+
+public class Test1 {
+
+	public static void main(String args[]) {
+		System.out.println("Git Demo Project");
+		boolean isAvail = true;
+		if(isAvail) {
+			System.out.println("Git Project Repo Available!!");
+		}else {
+			System.out.println("Git ProjectRepo not Available!!");
+		}
+	}
+
+}
